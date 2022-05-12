7cc81b45501424b7754a0790c80dfc6f0a72cbd7
Will <will.oliveira1@hotmail.com>
2022-05-12 00:09:27 -0300

    Atualização do InitialParams

--- CalculadoraDifal/Entities/InitialParams.cs
+++ CalculadoraDifal/Entities/InitialParams.cs
@@ -4,12 +4,13 @@ namespace CalculadoraDifal.Entities
 {
     class InitialParams
     {
-        public List<string> Tw1 = new List<string>();
-        public List<string> Tw2 = new List<string>();
-        string[] insideTax = { "AL", "BA", "GO", "MG", "PA", "PR", "PE", "RS", "RO", "SC", "SE", "TO" }; // Imposto Por Dentro
-        string[] directTax = { "AC", "AP", "AM", "CE", "DF", "ES", "MA", "MT", "MS", "PB", "PI", "RJ", "RN", "RR" }; //Imposto Direto
-        string twelvePercent = "MG PR RJ RS SC";
-        string sevenPercent = "AC AP AM CE DF ES MA MT MS PB PI RN RR AL BA GO PA PE RO SE TO";
+        public List<string> twelvePercentList, sevenPercenteList = new List<string>();
+        public List<string> insideTax, directTax = new List<string>();
+
+        string twelvePercentStates = "MG PR RJ RS SC";
+        string sevenPercentStates = "AC AP AM CE DF ES MA MT MS PB PI RN RR AL BA GO PA PE RO SE TO";
+        string insideTaxStates = "AL BA GO MG PA PR PE RS RO SC SE TO";     // Difal Por Dentro
+        string directTaxStates = "AC AP AM CE DF ES MA MT MS PB PI RJ RN RR";   // Difal Direto
 
         public Product Product { get; set; }
         public string State { get; set; }
@@ -26,16 +27,18 @@ namespace CalculadoraDifal.Entities
             
         private int IcmsTax(string state)
         {
-            Tw1.AddRange(twelvePercent.Split(" "));
-            Tw2.AddRange(sevenPercent.Split(" "));
-
-            if (Tw1.Contains(state))
+            twelvePercentList.AddRange(twelvePercentStates.Split(" "));
+            if (twelvePercentList.Contains(state))
             {
                 return 12;
             }
-            else if (Tw2.Contains(state))
+            else
             {
-                return 7;
+                sevenPercenteList.AddRange(sevenPercentStates.Split(" "));
+                if (sevenPercenteList.Contains(state))
+                {
+                    return 7;
+                }
             }
             return 0;
         }
